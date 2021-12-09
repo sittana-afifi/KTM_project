cdaae4d6c9ab340edb10f251318347a123caa2bb
EmanAbuBaker <eman.elsiddig@ms.sd.zain.com>
2021-12-09 13:07:54 +0000

    make user list view and CRUD with login required  option

--- accounts/views.py
+++ accounts/views.py
@@ -5,30 +5,28 @@ from django.contrib.auth.models import User
 from django.views.generic.edit import UpdateView , DeleteView , CreateView 
 from django.utils.translation import ugettext_lazy as _
 from django.urls import reverse_lazy
-from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
 
 
 
 # view the list of the users.
 
-class usersListView(PermissionRequiredMixin,generic.ListView):
-
-    permission_required = 'catalog.can_mark_returned'
+class usersListView(LoginRequiredMixin,generic.ListView):
     model = User
     template_name ='accounts/user_list.html'
 
 
 
 # view details of the specific user.
-class UserDetailView(generic.DetailView):
+class UserDetailView(LoginRequiredMixin,generic.DetailView):
     model = User
     template_name ='accounts/user_detail.html'
 
 
 
 # update specific user with specific fields.
-class UserUpdate(UpdateView):
+class UserUpdate(LoginRequiredMixin,UpdateView):
     model = User
     fields =['is_active','is_staff','is_superuser','groups','user_permissions']
     success_url = reverse_lazy('user_list')
@@ -36,14 +34,14 @@ class UserUpdate(UpdateView):
 
 
 # delete specific user.
-class UserDelete(DeleteView):
+class UserDelete(LoginRequiredMixin,DeleteView):
     model = User
     success_url = reverse_lazy('user_list')
 
 
 # Add new user.
 
-class UserCreate(CreateView):
+class UserCreate(LoginRequiredMixin,CreateView):
     model = User
     fields =['username','first_name','last_name','password','email','is_active','is_staff','is_superuser','groups','user_permissions','last_login','date_joined']
     initial = {'is_superuser': 'False' ,'date_joined':str(datetime.date.today()), 'last_login':str(datetime.date.today()) }
