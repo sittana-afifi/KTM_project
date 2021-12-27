04cf0dc223590b40dc74583f3087ecfa3efb8b16
sittana afifi <sittana.afifi@ms.sd.zain.com>
2021-12-27 12:32:27 +0000

    standerization navigation KTM project

--- accounts/models.py
+++ accounts/models.py
@@ -1,13 +1,17 @@
+from django.contrib.auth.base_user import BaseUserManager
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser , UserManager
+from django.urls import reverse
 
 
 
 # create new model to deal with user ,
 # this model is dynamics, we could extend it in demand.
-class Account (AbstractBaseUser):
+class Account (AbstractBaseUser,BaseUserManager):
 
-    
+
+    #def get_absolute_url(self):
+     #   return reverse('user-detail',args= [str(self.id)]) 
 
     def __str__(self):
         return self.username
